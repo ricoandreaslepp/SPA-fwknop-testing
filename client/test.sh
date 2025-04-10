a2982cf02b1e72cec69d3b9ae72ff051e2f39b8c
ricoandreaslepp <ricoandreaslepp@gmail.com>
2025-04-10 23:03:54 +0300

    obsolete version

---
+++ client/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# should result in filtered
+/usr/bin/nmap -p80 spa-server
+# send the SPA packet
+fwknop -n spa-server --rc-file /client/.fwknoprc -P icmp
+# should show open/closed
+/usr/bin/nmap -p80 spa-server
