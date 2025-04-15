d9f39d05e021192c7e14ae72b8aec7c6ce4850f3
ricoandreaslepp <ricoandreaslepp@gmail.com>
2025-04-15 22:17:20 +0300

    ICMP is not working for some reason

--- client/test.sh
+++ client/test.sh
@@ -3,6 +3,6 @@
 # should result in filtered
 /usr/bin/nmap -p80 spa-server
 # send the SPA packet
-fwknop -n spa-server --rc-file /client/.fwknoprc -P icmp
+fwknop -n spa-server --rc-file /client/.fwknoprc
 # should show open/closed
 /usr/bin/nmap -p80 spa-server
