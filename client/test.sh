02bfa8e4299a441db5c50509ecface600656edd8
ricoandreaslepp <ricoandreaslepp@gmail.com>
2025-04-16 23:59:21 +0300

    grep for port status

--- client/test.sh
+++ client/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # should result in filtered
-/usr/bin/nmap -p80 spa-server
+/usr/bin/nmap -p80 spa-server | grep filtered
 # send the SPA packet
 fwknop -n spa-server --rc-file /client/.fwknoprc
 # should show open/closed
-/usr/bin/nmap -p80 spa-server
+/usr/bin/nmap -p80 spa-server | grep closed
